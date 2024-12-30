bd8bad14d54132a0afd1ba266dad25118090d9ee
Hemarat Matra (Heam) <hemarat@shareinvestor.com>
2024-12-30 22:39:46 +0700

    config allow image content all domain

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
     images: {
         remotePatterns: [
           {
-            protocol: 'https',
-            hostname: 'fastly.picsum.photos'
+            protocol: "https",
+            hostname: "**",
           },
         ],
       },
